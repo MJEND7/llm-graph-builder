97d85e6e0f3e592246dd3e0f11b56f8159a71422
MJEND7 <markuss.cowburn@gmail.com>
2025-02-04 23:54:32 +1000

    chore: added back dashboard for local testing

--- frontend/src/Embed.tsx
+++ frontend/src/Embed.tsx
@@ -1,18 +1,33 @@
-import React from 'react';
-import PageLayout from './components/Layout/EmbedPageLayout.tsx';
-import { FileContextProvider } from './context/UsersFiles';
-import AlertContextWrapper from './context/Alert';
-import { MessageContextWrapper } from './context/UserMessages';
-
-const Embed: React.FunctionComponent = () => {
+import './App.css';
+import '@neo4j-ndl/base/lib/neo4j-ds-styles.css';
+import ThemeWrapper from './context/ThemeWrapper';
+import QuickStarter from './components/QuickStarter';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { APP_SOURCES } from './utils/Constants';
+import ErrorBoundary from './components/UI/ErrroBoundary';
+import { Toaster } from '@neo4j-ndl/react';
+const Embed: React.FC = () => {
   return (
-    <FileContextProvider>
-      <MessageContextWrapper>
-        <AlertContextWrapper>
-          <PageLayout />
-        </AlertContextWrapper>
-      </MessageContextWrapper>
-    </FileContextProvider>
+    <>
+      {APP_SOURCES != undefined && APP_SOURCES.includes('gcs') ? (
+        <ErrorBoundary>
+          <GoogleOAuthProvider clientId={process.env.VITE_GOOGLE_CLIENT_ID as string}>
+            <ThemeWrapper>
+              <QuickStarter />
+              <Toaster />
+            </ThemeWrapper>
+          </GoogleOAuthProvider>
+        </ErrorBoundary>
+      ) : (
+        <ErrorBoundary>
+          <ThemeWrapper>
+            <QuickStarter />
+            <Toaster />
+          </ThemeWrapper>
+        </ErrorBoundary>
+      )}
+    </>
   );
 };
+
 export default Embed;

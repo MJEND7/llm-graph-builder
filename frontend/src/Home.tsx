a5f00f0c3f06f7092606ca69f88f67715fa7df45
MJEND7 <markuss.cowburn@gmail.com>
2025-02-04 20:11:05 +1000

    feat: remove everything but embed

--- frontend/src/Home.tsx
+++ frontend/src/Home.tsx
@@ -1,33 +1,52 @@
 import './App.css';
 import '@neo4j-ndl/base/lib/neo4j-ds-styles.css';
-import ThemeWrapper from './context/ThemeWrapper';
-import QuickStarter from './components/QuickStarter';
-import { GoogleOAuthProvider } from '@react-oauth/google';
-import { APP_SOURCES } from './utils/Constants';
-import ErrorBoundary from './components/UI/ErrroBoundary';
-import { Toaster } from '@neo4j-ndl/react';
-const Home: React.FC = () => {
+//import ThemeWrapper from './context/ThemeWrapper';
+//import QuickStarter from './components/QuickStarter';
+//import { GoogleOAuthProvider } from '@react-oauth/google';
+//import { APP_SOURCES } from './utils/Constants';
+//import ErrorBoundary from './components/UI/ErrroBoundary';
+//import { Toaster } from '@neo4j-ndl/react';
+//const Home: React.FC = () => {
+//  return (
+//    <>
+//      {APP_SOURCES != undefined && APP_SOURCES.includes('gcs') ? (
+//        <ErrorBoundary>
+//          <GoogleOAuthProvider clientId={process.env.VITE_GOOGLE_CLIENT_ID as string}>
+//            <ThemeWrapper>
+//              <QuickStarter />
+//              <Toaster />
+//            </ThemeWrapper>
+//          </GoogleOAuthProvider>
+//        </ErrorBoundary>
+//      ) : (
+//        <ErrorBoundary>
+//          <ThemeWrapper>
+//            <QuickStarter />
+//            <Toaster />
+//          </ThemeWrapper>
+//        </ErrorBoundary>
+//      )}
+//    </>
+//  );
+//};
+//
+//export default Home;
+
+import React from 'react';
+import PageLayout from './components/Layout/EmbedPageLayout.tsx';
+import { FileContextProvider } from './context/UsersFiles';
+import AlertContextWrapper from './context/Alert';
+import { MessageContextWrapper } from './context/UserMessages';
+
+const Embed: React.FunctionComponent = () => {
   return (
-    <>
-      {APP_SOURCES != undefined && APP_SOURCES.includes('gcs') ? (
-        <ErrorBoundary>
-          <GoogleOAuthProvider clientId={process.env.VITE_GOOGLE_CLIENT_ID as string}>
-            <ThemeWrapper>
-              <QuickStarter />
-              <Toaster />
-            </ThemeWrapper>
-          </GoogleOAuthProvider>
-        </ErrorBoundary>
-      ) : (
-        <ErrorBoundary>
-          <ThemeWrapper>
-            <QuickStarter />
-            <Toaster />
-          </ThemeWrapper>
-        </ErrorBoundary>
-      )}
-    </>
+    <FileContextProvider>
+      <MessageContextWrapper>
+        <AlertContextWrapper>
+          <PageLayout />
+        </AlertContextWrapper>
+      </MessageContextWrapper>
+    </FileContextProvider>
   );
 };
-
-export default Home;
+export default Embed;
